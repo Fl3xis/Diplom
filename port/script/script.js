485ff1e5e794debbda0f5b3b79e0c2c91eb42a60
Fl3xis <60088701+Fl3xis@users.noreply.github.com>
2022-08-28 11:11:02 +0300

    Add files via upload

---
+++ port/script/script.js
@@ -0,0 +1,44 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+console.log(header)
+window.onscroll = function() {
+    const top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+// collapse navbar after click on small devices
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
+})
+
+/* marq */
+
+var wrapper = document.querySelector('.marquee-wrapper'),
+    marquee = document.querySelector('.marquee'),   
+    wrapperWidth = wrapper.offsetWidth,
+    marqueeWidth = marquee.scrollWidth;
+    
+function move() {
+  var currentTX = getComputedStyle(marquee).transform.split(',');
+  if( currentTX[4] === undefined ) {
+    currentTX = -1;
+  } else {
+    currentTX = parseFloat(currentTX[4]) - 1;
+  }
+  
+  if(-currentTX >= marqueeWidth) {
+    marquee.style.transform = 'translateX(' + wrapperWidth + 'px)';
+  
+  } else {
+    marquee.style.transform = 'translateX(' + currentTX + 'px)';
+  }
+}
+
+var interval = setInterval(move, 10);
